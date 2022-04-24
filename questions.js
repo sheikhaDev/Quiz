d2d17b8f4c7242ecaec860299350065fdc0a2af3
sheikhadev <101664866+sheikhaDev@users.noreply.github.com>
2022-04-25 02:52:54 +0400

    main

---
+++ questions.js
@@ -0,0 +1,63 @@
+// Questions as an array of Objects
+var questions = [
+    {
+      title: '1-What does HTML stand for?',
+      choices: ['Hyper Text Markup language', 'Hyperlinks and Text Markup language', 'Home Markup language', 'Home Tool Markup language'],
+      answer: 'Hyper Text Markup language'
+    },
+    {
+      title: '2-Who is making the Web standards?',
+      choices: ['Mozila', 'Google', 'WWW', 'Microsoft'],
+      answer: 'WWW'
+    },
+    {
+      title: '3-Choose the correct HTML element for the largest heading?',
+      choices: ['h5', 'head', 'h1', 'h6'],
+      answer: 'h1'
+    },
+    {
+      title: '4-What is the correct HTML element for inserting a line break?',
+      choices: ['break', 'br', 'goto', 'hr'],
+      answer: 'br'
+    },
+    {
+      title: '5-Choose the correct HTML element to define important text?',
+      choices: ['<important>', '<strong>', '<b>', '<p>'],
+      answer: '<strong>'
+    },
+    {
+      title: '6-What is the correct HTML for making a drop-down list?',
+      choices: ['<select>', '<input type="dropdown">', '<input type="list">', '<list>'],
+      answer: '<select>'
+    },
+    {
+      title: '7-Choose the correct HTML element to define emphasized text?',
+      choices: ['<italic>', '<em>', '<i>', '<emphe>'],
+      answer: '<em>'
+    },
+    {
+      title: '8-In HTML, which attribute is used to specify that an input field must be filled out?',
+      choices: ['placeholder', 'validate', 'required', 'formvalidate'],
+      answer: 'required'
+    },
+    {
+      title: '9-Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?',
+      choices: ['src', 'title', 'href', 'alt'],
+      answer: 'alt'
+    },
+    {
+      title: '10-Which HTML element defines navigation links?',
+      choices: ['<navigation>', '<nav>', '<navigate>', '<link>'],
+      answer: '<nav>'
+    },
+    {
+      title: '11-Which HTML element defines the title of a document?',
+      choices: ['<meta>', '<head>', '<title>', '<header>'],
+      answer: '<title>'
+    },
+    {
+      title: '12-How can you make a numbered list?',
+      choices: ['<ol>', '<list>', '<il>', '<ul>'],
+      answer: '<ol>'
+    },
+  ];
